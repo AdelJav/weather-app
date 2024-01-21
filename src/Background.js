c5798af56bdf93d934a0feb5bc42732def6363a6
AdelJav <adeljavorska@gmail.com>
2024-01-21 15:42:22 +0100

    Background js

---
+++ src/Background.js
@@ -0,0 +1,8 @@
+import React from "react";
+import autumn2 from "./images/autumn2.jpg";
+
+import "./Background.css";
+
+export default function Background() {
+  return <img src={autumn2} alt="autumn2" className="backimage" />;
+}
