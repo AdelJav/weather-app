b932e2c0bc5cddb2d1e015c0a9be5fdc70b35b7b
AdelJav <adeljavorska@gmail.com>
2024-01-24 19:32:59 +0100

    pictures added, background season dependend

--- src/Background.js
+++ src/Background.js
@@ -1,8 +1,24 @@
 import React from "react";
 import winter2 from "./images/winter2.jpg";
+import spring2 from "./images/spring2.jpg";
+import summer2 from "./images/summer2.jpg";
+import autumn2 from "./images/autumn2.jpg";
 
 import "./Background.css";
 
 export default function Background() {
-  return <img src={winter2} alt="winter2" className="backimage" />;
+  const d = new Date();
+  let month = d.getMonth();
+
+  if (month < 3) {
+    return <img src={winter2} alt="winter2" className="backimage" />;
+  }
+  if (month < 6) {
+    return <img src={spring2} alt="spring2" className="backimage" />;
+  }
+  if (month < 9) {
+    return <img src={summer2} alt="summer2" className="backimage" />;
+  } else {
+    return <img src={autumn2} alt="autumn2" className="backimage" />;
+  }
 }
