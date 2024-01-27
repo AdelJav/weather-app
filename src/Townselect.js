ce2a5cbda441bedff292db4ecf8b2f739436b4c6
AdelJav <adeljavorska@gmail.com>
2024-01-27 11:59:12 +0100

    apiKey defined

--- src/Townselect.js
+++ src/Townselect.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Townselect.css";
+import axios from "axios";
 
 export default function Townselect() {
+  let apiKey = "9347o49b938f15b43a2at5d07eb97eb4";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${input}&appid=${apiKey}&units=metric`;
+
   return (
     <form>
       <input
